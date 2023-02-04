67f5fc375bf23e74f50898447f7b85ed5afc9004
katie820 <katie82083@yahoo.com>
2023-02-03 20:23:12 -0800

    Completed java lab 003

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -10,5 +10,15 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+        System.out.println(number + "°F");
+        float celcius = ((number - 32) * 5/9);
+        System.out.printf("%.6f",celcius);
+        System.out.println("°C");
+        float cel = celcius +2;
+        String result = String.format("%.6f", cel);
+        int c = ((number - 32) * 5/9);
+        System.out.println(c + "°C");
+        System.out.println("If it were 2° warmer it would be: " + result + "°C");
+
     }
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -1,11 +1,23 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Katie Logefeil
+ *
+ * @since Version 1.0
+ */
+
 // Import the Scanner tool used for reading (All external packages/libraries/apis are imported similarly this)
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class MrRoboto {
     /**
      * Teaching Mr. Roboto how to listen to us.
+     *
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
+
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
@@ -15,5 +27,9 @@ public class MrRoboto {
 
         System.out.print("My name is Mr. Roboto, are you Kilroy? ");
         // Write your program here
+        String x = scanner.nextLine();
+        System.out.println(domo + x);
+
+
     }
 }
